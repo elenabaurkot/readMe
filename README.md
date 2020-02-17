d93ef62b019d345ea7c1e42a95b5edb59e64c3ab
elenabaurkot <elenabaurkot@gmail.com>
2020-02-17 00:06:48 -0500

    finalizations and readme created

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# readMe
+# README generator 
+ The purpose of this project is to create a program to help users easily develop a readme file for their Github projects. Users are prompted with a series of questions and after typing in their answers a formatted README file is generated. 
+
+## Table of Contents
+<ol>
+    <li><a href="#installation">Installation</a></li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+</ol>
+
+## Installation 
+<a name="installation"></a>
+ <ul>
+    <li>Inquirer </li>
+    <li>Axios </li>
+ </ul>
+
+## Usage 
+<a name="usage"></a>
+ At the top of your program be sure to include:<br>
+ <ul>
+    <li>var fs = require("fs");</li>
+    <li>var inquirer = require("inquirer"); </li>
+    <li>const axios = require("axios");</li>
+ </ul>
+ You will need to run "npm init" in your terminal and then "npm install inquirer" and "npm install axios"
+
+## License 
+<a name="license"></a>
+ &copy; Elena Baurkot 
+
+## Contributing 
+<a name="contributing"></a>
+ If you would like to contribute feel free to clone the link and edit as you please: git@github.com:elenabaurkot/readMe.git
+<br>
+<br>
+Github email: Elenabaurkot@gmail.com <br>
+<img src="https://avatars2.githubusercontent.com/u/30561514?v=4" alt= "avatar pic" height="50px" width="50px">
--- index.js
+++ index.js
@@ -1,70 +1,88 @@
 var fs = require("fs");
 var inquirer = require("inquirer");
+const axios = require("axios");
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your GitHub username?",
-      name: "username"
-      //   think this needs to be an ajax call
-    },
-    {
-      type: "input",
-      message: "What is the project name?",
-      name: "title"
-    },
-    {
-      type: "input",
-      message: "Write a description of the project",
-      name: "description"
-    },
-    {
-      type: "input",
-      message: "Describe what installations are needed to run the project",
-      name: "installation"
-    },
-    {
-      type: "input",
-      message:
-        "What are the step-by-step instructions a user needs to use the program?",
-      name: "usage"
-    },
-    {
-      type: "input",
-      message: "Tell how the program is licensed",
-      name: "license"
-    },
-    {
-      type: "input",
-      message:
-        "State if you are open to contributions and what your requirements are for accepting them.",
-      name: "contributions"
-    },
-    {
-      type: "input",
-      message:
-        "Explain how to run the automated tests for the program. Tell the user what they test and why.",
-      name: "tests"
-    },
-    {
-        type: "input",
-        message:"Write any common questions a user might have here and their answers",
-        name: "FAQ"
-      }
-  ])
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username"
+  },
+  {
+    type: "input",
+    message: "What is your GitHub email?",
+    name: "email"
+  },
+  {
+    type: "input",
+    message: "What is the project name?",
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "Write a description of the project",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "Describe what installations are needed to run the project",
+    name: "installation"
+  },
+  {
+    type: "input",
+    message:
+      "What are the step-by-step instructions a user needs to use the program?",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "Tell how the program is licensed",
+    name: "license"
+  },
+  {
+    type: "input",
+    message:
+      "State if you are open to contributions and what your requirements are for accepting them.",
+    name: "contributions"
+  },
+  {
+    type: "input",
+    message:
+      "Explain how to run the automated tests for the program. Tell the user what they test and why.",
+    name: "tests"
+  },
+  {
+    type: "input",
+    message:
+      "Write any common questions a user might have here and their answers",
+    name: "FAQ"
+  }
+])
 
   .then(function(answer) {
+
+    // Call to get github avatar
+    axios
+    .get(`https://api.github.com/users/${answer.username}`)
+    .then(function(res) {
+      console.log(res.data);
+    // link to github avatar
+    let avatar = res.data.avatar_url; 
+    console.log(avatar);
+
+    // write to file Readme1.md
     fs.writeFile(
       "ReadMe1.md",
-      // need to write the callback here for the github username
         `# ${answer.title} \n ${answer.description} \n` +
         `## Installation \n ${answer.installation} \n` +
         `## Usage \n ${answer.usage} \n` +
         `## License \n ${answer.license} \n` +
         `## Contributing \n ${answer.contributions} \n` +
         `## Tests \n ${answer.tests} \n` +
-        `## FAQ \n ${answer.FAQ}`,
+        `## FAQ \n ${answer.FAQ} \n` +
+        `Github email: ${answer.email} \n \n` +
+        // github avatar image
+        `<img src="${avatar}" alt= "avatar pic" height="50px" width="50px">`,
 
       function(err) {
         if (err) {
@@ -73,4 +91,5 @@ inquirer
         console.log("Success!");
       }
     );
-  });
+  })
+})
